2700cd9c6bc759f7ccb3a149bc3c2f23cc12e330
Mahdy <damahdy@gmail.com>
2024-08-02 10:40:45 +0200

    Update admin.py

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,5 @@
+
 from django.contrib import admin
 
 # Register your models here.
+#This code imports Django's admin module, which is used to register models so they can be managed through the Django admin interface.
