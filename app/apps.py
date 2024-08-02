9c7fe7c13b2e67b37e2d70a585626650226d5466
Mahdy <damahdy@gmail.com>
2024-08-02 10:43:44 +0200

    Update apps.py

--- app/apps.py
+++ app/apps.py
@@ -1,6 +1,11 @@
+#This file defines the configuration settings for a Django application named 'app'
+#specifying that the default primary key field type for models should be BigAutoField
+
 from django.apps import AppConfig
 
-
+# Define a configuration class for the Django application
 class AppConfig(AppConfig):
+    # Set the default primary key field type for models to BigAutoField
     default_auto_field = 'django.db.models.BigAutoField'
+    # Specify the name of the application
     name = 'app'
