4ff96e3c25aa8d7ed8d9d448c86bbf81bf694057
Mahdy <damahdy@gmail.com>
2024-08-11 15:09:58 +0200

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render  # Import the render function from Django's shortcuts module
 
-
+# Define a view function named 'home' that takes an HTTP request object as a parameter
 def home(request):
+    # The render function combines a given template with a context dictionary and returns an HttpResponse object
+    # In this case, it renders the 'index.html' template without any additional context
     return render(request, template_name='index.html')
