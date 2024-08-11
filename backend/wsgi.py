0c0f9a623fdd57c3f1cdedf38ea5b6505007e803
Mahdy <damahdy@gmail.com>
2024-08-11 15:10:58 +0200

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-
+# Set the default settings module for the 'DJANGO_SETTINGS_MODULE' environment variable.
+# This tells Django which settings file to use for the application.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+# Get the WSGI application callable for the project.
+# This application object can be used by a WSGI server to forward requests to your Django application.
 application = get_wsgi_application()
